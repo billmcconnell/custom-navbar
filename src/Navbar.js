b515776b8a62dd20eef1b571d0e1f83f4be90884
billmcconnell <44976673+billmcconnell@users.noreply.github.com>
2023-07-04 22:48:08 -0400

    update to Navbar style color

--- src/Navbar.js
+++ src/Navbar.js
@@ -12,16 +12,24 @@ const Navbar = () => {
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
           <ul className="navbar-nav mr-auto mb-2 mb-lg-0">
             <li className="nav-item">
-              <a className="nav-link active" aria-current="page"> <Link to="/">Home</Link></a>
+              <a className="nav-link" aria-current="page"> <Link to="/" style={{
+                    color: "white",
+               }}>Home</Link></a>
             </li>
             <li className="nav-item">
-              <div className="nav-link active" aria-current="page"> <Link to="/SignIn">Sign In</Link></div>
+              <div className="nav-link" aria-current="page"> <Link to="/SignIn" style={{
+                    color: "white",
+                }}>Sign In</Link></div>
             </li>
             <li className="nav-item">
-              <div className="nav-link active" aria-current="page"> <Link to="/Mission">Mission</Link></div>
+              <div className="nav-link" aria-current="page"> <Link to="/Mission" style={{
+                    color: "white",
+                }}>Mission</Link></div>
             </li>
             <li className="nav-item">
-              <div className="nav-link active" aria-current="page"> <Link to="/About">About</Link></div>
+              <div className="nav-link" aria-current="page"> <Link to="/About" style={{
+                    color: "white",
+                }}>About</Link></div>
             </li>
           </ul>
         </div>
@@ -29,37 +37,5 @@ const Navbar = () => {
     </nav>
   );
 }
-/*
-<nav className="navbar navbar-expand-lg navbar-dark bg-dark sticky-top">
-  <div className="container-fluid">
-    <a className="navbar-brand" href="#">Navbar</a>
-    <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span className="navbar-toggler-icon"></span>
-    </button>
-    <div className="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul className="navbar-nav mr-auto mb-2 mb-lg-0">
-        <li className="nav-item">
-          <a className="nav-link active" aria-current="page" href="#">Home</a>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link" href="#">Link</a>
-        </li>
-        <li className="nav-item dropdown">
-          <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-expanded="false">
-            Dropdown
-          </a>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link disabled" href="#" tabIndex="-1" aria-disabled="true">Disabled</a>
-        </li>
-      </ul>
-    </div>
-  </div>
-</nav>
-
-
-
-);
-}*/
 
 export default Navbar;
